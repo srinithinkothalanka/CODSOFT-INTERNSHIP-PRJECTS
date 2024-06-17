5244ec11cd316f0a3b86f51ea6c4ea68055cb1c4
srinithinkothalanka <kothalankasrinithin@gmail.com>
2024-06-17 10:37:52 +0530

    first commit

---
+++ GuessingGame.java
@@ -0,0 +1,48 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean playAgain = true;
+        
+        while (playAgain) {
+            int numberToGuess = random.nextInt(100) + 1;
+            int numberOfAttempts = 0;
+            int maxAttempts = 10;
+            boolean hasGuessedCorrectly = false;
+
+            System.out.println("Guess the number between 1 and 100. You have " + maxAttempts + " attempts.");
+
+            while (numberOfAttempts < maxAttempts && !hasGuessedCorrectly) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                numberOfAttempts++;
+
+                if (userGuess == numberToGuess) {
+                    System.out.println("Congratulations! You guessed the number correctly in " + numberOfAttempts + " attempts.");
+                    hasGuessedCorrectly = true;
+                } else if (userGuess < numberToGuess) {
+                    System.out.println("Your guess is too low.");
+                } else {
+                    System.out.println("Your guess is too high.");
+                }
+            }
+
+            if (!hasGuessedCorrectly) {
+                System.out.println("Sorry, you've used all your attempts. The number was: " + numberToGuess);
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String userResponse = scanner.next();
+            playAgain = userResponse.equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Thank you for playing!");
+        scanner.close();
+    }
+}
+
